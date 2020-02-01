1db536e223701a09b1be906cadcd850d59fdec07
a-a7 <a.s.arora0407@gmail.com>
2020-02-01 10:10:41 -0500

    WIP: The tutorial for asp.net core with mongoDB

---
+++ backend/RecipesApi/RecipesApi/Models/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RecipesApi.Models
+{
+    public class Recipe
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
