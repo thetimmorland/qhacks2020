1bb62e9f6efb5eb21084a02d2ece94a797f3eef4
Timothy Morland <thetimmorland@gmail.com>
2020-01-31 22:31:16 -0500

    fixup! setup basic material-ui and react router

--- web-app/src/Editor.js
+++ web-app/src/Editor.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Editor = () => {
     return (<p>Editor</p>)
   }
--- web-app/src/Recipe.js
+++ web-app/src/Recipe.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Recipe = () => {
     let { recipeId } = useParams()
     return (<p>Recipe {recipeId}</p>)
