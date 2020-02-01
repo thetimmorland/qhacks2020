82ad68ecac1ae3bc4b40e5cd936b4ad0b96f68f6
a-a7 <a.s.arora0407@gmail.com>
2020-02-01 15:06:57 -0500

    Backend code wip

---
+++ backend/Attempt2/RecipesApi/RecipesApi/Models/Recipe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace RecipesApi.Models
+{
+    public class Recipe
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string RecipeName { get; set; }
+
+        public string Notes { get; set; }
+
+        public Ingredients Ingredients { get; set; }
+
+        public int Rating { get; set; }
+    }
+    public class Ingredients
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string Units { get; set; }
+    }
+}
