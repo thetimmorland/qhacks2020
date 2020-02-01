f73a3ffb60f61fc096395d29ac0366e6089ebbbb
Timothy Morland <thetimmorland@gmail.com>
2020-02-01 01:20:30 -0500

    intial mockup for Recipe.js

--- web-app/src/Recipe.js
+++ web-app/src/Recipe.js
@@ -5,47 +5,71 @@ import {
 } from "react-router-dom";
 
 import {
+    Container,
     Box,
     Grid,
     Typography,
     Paper,
 } from "@material-ui/core";
 
+const PaperList = props =>
+    <Paper>
+        <Box p={2}>
+            <Typography variant="h5" color="primary" gutterBottom>
+                {props.header}
+            </Typography>
+            <Container maxWidth="md">
+                {props.children}
+            </Container>
+        </Box>
+    </Paper>
+
 export default function Recipe() {
     let { recipeId } = useParams()
 
     const [recipe, setRecipe] = useState({
         ingredients: [
-            { name: "eggs", amount: 2, unit: "" },
-            { name: "flour", amount: 2, unit: "cups" },
+            { name: "Eggs", amount: 2, unit: "" },
+            { name: "Flour", amount: 2, unit: "Cups" },
         ],
         instructions: [
             "Add flour.",
             "Create well in flour.",
             "Crack egg in well.",
-        ]
+        ],
+        notes: "This recipe is very good. I make it all the time with my kids!"
     })
 
     return (
-        <Box>
-            <Grid container>
-                <Grid object>
-                    <Grid container>
-                    </Grid>
-                </Grid>
-                <Paper>
-                    <Box m={2}>
-                        <Grid object>
-                            <Grid container direction="column">
-                                <Typography variant="h6">Ingredients</Typography>
+        <Box p={2}>
+            <Container maxWidth="md">
+                <Grid container direction="column" spacing={2}>
+                    <Grid item>
+                        <PaperList header="Ingredients">
+                            <Grid container>
                                 {recipe.ingredients.map(ingredient =>
-                                    <Typography>{ingredient.name} {ingredient.amount}</Typography>
+                                    <Grid item xs={12} sm={6}>
+                                        <Typography>
+                                            <li>{ingredient.name}: {ingredient.amount} {ingredient.unit}</li>
+                                        </Typography>
+                                    </Grid>
                                 )}
                             </Grid>
-                        </Grid>
-                    </Box>
-                </Paper>
-            </Grid>
+                        </PaperList>
+                    </Grid>
+                    <Grid item>
+                        <PaperList header="Instructions">
+                            <ol>
+                                {recipe.instructions.map(instruction =>
+                                    <Typography>
+                                        <li>{instruction}</li>
+                                    </Typography>
+                                )}
+                            </ol>
+                        </PaperList>
+                    </Grid>
+                </Grid>
+            </Container>
         </Box>
     )
 }
