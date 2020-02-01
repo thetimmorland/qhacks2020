317f1c4293d7a35d85e273552d543f3d0709e65b
Timothy Morland <thetimmorland@gmail.com>
2020-01-31 23:09:41 -0500

    Wip Recipe page

--- web-app/src/Recipe.js
+++ web-app/src/Recipe.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Recipe = () => {
+import {
+    useParams,
+} from "react-router-dom";
+
+import {
+    Box,
+    Grid,
+    Typography,
+    Paper,
+} from "@material-ui/core";
+
+export default function Recipe() {
     let { recipeId } = useParams()
-    return (<p>Recipe {recipeId}</p>)
-  }
+
+    const [recipe, setRecipe] = useState({
+        ingredients: [
+            { name: "eggs", amount: 2, unit: "" },
+            { name: "flour", amount: 2, unit: "cups" },
+        ],
+        instructions: [
+            "Add flour.",
+            "Create well in flour.",
+            "Crack egg in well.",
+        ]
+    })
+
+    return (
+        <Box>
+            <Grid container>
+                <Grid object>
+                    <Grid container>
+                    </Grid>
+                </Grid>
+                <Paper>
+                    <Box m={2}>
+                        <Grid object>
+                            <Grid container direction="column">
+                                <Typography variant="h6">Ingredients</Typography>
+                                {recipe.ingredients.map(ingredient =>
+                                    <Typography>{ingredient.name} {ingredient.amount}</Typography>
+                                )}
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Paper>
+            </Grid>
+        </Box>
+    )
+}
