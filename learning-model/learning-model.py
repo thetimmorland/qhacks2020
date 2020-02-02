e8f9ec97c3a7c52a0bc19c7da6df8b9045eb73de
JasperSchaffer <44Jasper44@gmail.com>
2020-02-01 19:11:57 -0500

    Update learning-model.py

--- learning-model/learning-model.py
+++ learning-model/learning-model.py
@@ -174,7 +174,7 @@ def createRecipeVariations(exampleRecipe, numberOfVariations):
         za = random.random()
         if (za > 0.33):
             index = random.randint(0,(len(recipeIngredients))-1)
-            while( recipeIngredients[index]["unit"]==""):
+            while( recipeIngredients[index]["unit"]=="" or recipeIngredients[index]["unit"][0]==" " ):
                 index = random.randint(0,(len(recipeIngredients))-1)
             if(index==2):
                 ratingChange = 1
@@ -225,13 +225,13 @@ def createRecipeVariations(exampleRecipe, numberOfVariations):
 
         finalObject = {}
         finalObject["recipe"] = temporaryRecipe
-        if ratingChange == 0:
-            finalObject["rating"] = 3
-        elif ratingChange==-1:
-            finalObject["rating"] = 1
-        else:
-            finalObject["rating"] = 5
-        # finalObject["rating"] = random.randint(0,5)
+        # if ratingChange == 0:
+        #     finalObject["rating"] = 3
+        # elif ratingChange==-1:
+        #     finalObject["rating"] = 1
+        # else:
+        #     finalObject["rating"] = 5
+        finalObject["rating"] = random.randint(0,5)
 
         allVariations.append(finalObject)
 
@@ -241,47 +241,37 @@ def createRecipeVariations(exampleRecipe, numberOfVariations):
 # end createRecipeVariations
 
 if __name__ == "__main__":
-    ex = {
-        "name":
-        "Cake",
-        'notes':
-        "This recipe is very good. I make it all the time with my kids!",
-        'ingredients': [
-            {
-                "name": "Eggs",
-                "amount": 20,
-                "unit": ""
-            },
-            {
-                "name": "Flour",
-                "amount": 20,
-                "unit": "Cups"
-            },
-            {
-                "name": "Sugar",
-                "amount": 20,
-                "unit": "Cups"
-            },
-            {
-                "name": "Baking Soda",
-                "amount": 20,
-                "unit": "Teaspoons"
-            },
-        ],
-        "instructions": [
-            "Add flour.",
-            "Create well in flour.",
-            "Crack egg in well.",
-            "heat oven to 200.0 C",
-            "cook for 20.0 min",
-        ],
-    }
-    example = {"recipe": ex, "rating": 5}
+    porkChop = {
+
+        "name"  :   "Pork Chop",
+            "notes" :   "Yummy yummy in my tummy",
+            "ingredients"   :  [
+                {"name" : "salt", "amount": 118, "unit": "ml"},
+                {"name" : "water", "amount": 710, "unit": "ml"},
+                {"name" : "pork chops", "amount": 2, "unit": ""},
+                {"name" : "brown sugar", "amount": 60, "unit": "ml"},
+                {"name" : "butter", "amount": 30, "unit": "ml"},
+                {"name" : "thyme", "amount": 4, "unit": " sprigs"},
+                {"name" : "garlic", "amount": 2, "unit": "cloves"},
+            ],
+            "instructions": [
+                "Tenderize porkchops with a fork",
+                "Place salt, sugar, and water in a plastic bag and stire until mixed",
+                "Put porkchops in bag, and let marinate for 30 minutes",
+                "Remove porkchops from bag and dry with paper towel",
+                "Heat pan to medium high heat",
+                "Cook on each side for 1 minute",
+                "Reduce heat to medium and continue cooking for 9 minutes, flipping the chops every minute",
+                "Remove the pan from the stove, add butter, garlic, and thyme, basting the pork chops",
+                "Let the porkchops rest in the pan for 5 minutes",
+            ],
+}
+    example = {"recipe": porkChop, "rating": 5}
     recipeVariations = createRecipeVariations(example, 100)
 
-    # newRecipe = calculateNewMasterRecipe(recipeVariations)
-    # print(calculateNewMasterRecipe(recipeVariations))
-    changeRecipe():
+     
+    print(calculateNewMasterRecipe(recipeVariations))
+    #changeRecipe()
 # print(recipeVariations)
 #newRecipe = calculateNewMasterRecipe(recipeVariations)
 # print(newRecipe)
